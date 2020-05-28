3449ceb2ea44b24bcc8d5c29128bc1e9dc9eb594
Evan <eokeefe3@wisc.edu>
2020-05-28 15:17:37 -0500

    Implemented tournament win system
    Next step build C4 AI

--- Connect4/c4.py
+++ Connect4/c4.py
@@ -1,15 +1,18 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
 import numpy as np
 import random, time
 
 class Connect4:
-    def __init__(self, rows, cols):
+    def __init__(self, rows, cols, win_len):
         self.board = np.zeros((rows,cols), dtype=int)
         self.rows = rows
         self.cols = cols
+        self.win_len = win_len
+        self.p1_wins = 0
+        self.p2_wins = 0
     
+    def clear_board(self):
+        self.board = np.zeros((self.rows,self.cols), dtype=int)
+        print('Clearing the board')
     def add_chip(self, col, player):
         for idx, row in enumerate(self.board[:, col]):
             if row == 0:
@@ -28,29 +31,31 @@ class Connect4:
     def check_winner(self):
         checks = []
         for row in range(self.rows):
-            for i in range(self.cols-3):
-                checks.append(self.board[row, i:i+4])
+            for i in range(self.cols-(self.win_len-1)):
+                checks.append(self.board[row, i:i+self.win_len])
 
         for col in range(self.cols):
-            for i in range(self.rows-3):
-                checks.append(self.board[i:i+4, col])
+            for i in range(self.rows-(self.win_len-1)):
+                checks.append(self.board[i:i+self.win_len, col])
 
-        for i in range(self.rows-3):
-            for j in range(self.cols-3):
-                checks.append(self.board[i:i+4].diagonal(j)[:4])
-                checks.append(np.fliplr(self.board[i:i+4]).diagonal(j))
-                checks.append(np.flipud(self.board[i:i+4]).diagonal(j))
-                checks.append(np.flipud(np.fliplr(self.board[i:i+4])).diagonal(j))
+        for i in range(self.rows-(self.win_len-1)):
+            for j in range(self.cols-(self.win_len-1)):
+                checks.append(self.board[i:i+self.win_len].diagonal(j)[:self.win_len])
+                checks.append(np.fliplr(self.board[i:i+self.win_len]).diagonal(j))
+                checks.append(np.flipud(self.board[i:i+self.win_len]).diagonal(j))
+                checks.append(np.flipud(np.fliplr(self.board[i:i+self.win_len])).diagonal(j))
 
 
-        p1_win = [1, 1, 1, 1]
-        p2_win = [2, 2, 2, 2]
+        p1_win = [1] * self.win_len
+        p2_win = [2] * self.win_len
         for item in checks: 
             if np.array_equal(item, p1_win):
                 self.winner = 'Player 1'
+                self.p1_wins += 1
                 return True
             elif np.array_equal(item, p2_win):
                 self.winner = 'Player 2'
+                self.p2_wins += 1
                 return True
         return False
 
@@ -64,7 +69,7 @@ class Connect4:
                 player = 2
             
             print(f"It is Player {player}'s turn to play")
-            inp = int(input('Which column would you like to play? (enter 0-6) '))
+            inp = int(input(f'Which column would you like to play? (enter 0-{self.cols-1}) '))
             
             if self.valid_input(inp):
                 self.board = self.add_chip(inp, player)
@@ -74,7 +79,8 @@ class Connect4:
             else:
                 print('Not a valid move. Try again')
             game_over = self.check_winner() 
-        print(f"{self.winner} won the game!") 
+        print(f"{self.winner} won the game!")
+        self.clear_board()
 
     def start_game(self):
         #cpu = input('Would you like to play PvP or PvCPU? (enter p or cpu) ').lower().strip()
@@ -93,6 +99,11 @@ class Connect4:
         print('\n')
         
         while num_game > 0:
+            if self.p1_wins > num_game:
+                return print('Player 1 won the tournament!')
+            if self.p2_wins > num_game:
+                return print('Player 2 won the tournament!')
+
             start = input('Would you like to start the game? (y/n) ').lower().strip()
             if start == 'y':
                 print('\n')
@@ -102,7 +113,5 @@ class Connect4:
                 continue
             num_game -= 1
 
-new = Connect4(6,7)
+new = Connect4(6, 7, 4)
 new.start_game()
-
-# %%
